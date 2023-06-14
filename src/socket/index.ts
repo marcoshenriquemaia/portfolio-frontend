1adeccae19cf845d2a86dfa5f08ee7c52eb90e49
Marcos Maia <henriquemarcosmaia@gmail.com>
2023-06-14 00:47:31 -0300

    change host name

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,6 +1,6 @@
 import { io } from 'socket.io-client'
 
-export const socket = io('https://worldapi.revele.live', {
+export const socket = io('https://worldapi.marcosmaia.dev', {
   transports: ['websocket'],
 })
 
