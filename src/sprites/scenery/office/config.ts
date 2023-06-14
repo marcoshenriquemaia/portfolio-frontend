86de43b7b23265754a791ce135a94c745722c6bd
Marcos Maia <henriquemarcosmaia@gmail.com>
2023-06-13 23:31:17 -0300

    deploy

--- src/sprites/scenery/office/config.ts
+++ src/sprites/scenery/office/config.ts
@@ -2,7 +2,6 @@ import { SceneryConfig } from '../../../interfaces/scenery'
 
 export const sceneryConfig: SceneryConfig = {
   path: 'src/sprites/scenery/office/assets/office.png',
-  // path: 'src/sprites/characters/ian/assets/ian.png',
   fullSize: {
     width: 1364,
     height: 2157,
